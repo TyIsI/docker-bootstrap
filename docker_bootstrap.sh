68b64ba3d49bda9784912f839890e68655db462b
Ty <tycho.eggen@gmail.com>
2026-03-31 13:15:15 -0700

    feat: fine-tuned directory and script execution order

--- docker_bootstrap.sh
+++ docker_bootstrap.sh
@@ -10,33 +10,35 @@ BOOTSTRAP_SHELL="${BOOTSTRAP_SHELL:-$(realpath /bin/sh)}"
 SELF_SHELL=$(realpath /proc/$$/exe)
 
 if [ "${BOOTSTRAP_SHELL}" != "${SELF_SHELL}" ]; then
-	echo "Bootstrap_shell (${BOOTSTRAP_SHELL}) is not matching parent shell (${SELF_SHELL})."
-	echo "Respawing!"
-	exec "${BOOTSTRAP_SHELL}" "$0" "$*"
+    echo "Bootstrap_shell (${BOOTSTRAP_SHELL}) is not matching parent shell (${SELF_SHELL})."
+    echo "Respawing!"
+    exec "${BOOTSTRAP_SHELL}" "$0" "$*"
 fi
 
 if [ "${BOOTSTRAP_BYPASS}" = "0" ]; then
-	BOOTSTRAP_DIRS="${BOOTSTRAP_DIRS:-}"
+    BOOTSTRAP_DIRS="${BOOTSTRAP_DIRS:-}"
 
-	BOOTSTRAP_DIRS="${BOOTSTRAP_DIRS} ${BOOTSTRAP_DIR:-/etc/bootstrap.d}"
+    BOOTSTRAP_DIRS="${BOOTSTRAP_DIRS} ${BOOTSTRAP_DIR:-/etc/bootstrap.d}"
 
-	if [ "${CUSTOM_BOOTSTRAP_DIR}" != "" ]; then
-		BOOTSTRAP_DIRS="${BOOTSTRAP_DIRS} ${CUSTOM_BOOTSTRAP_DIR}"
-	fi
+    if [ "${CUSTOM_BOOTSTRAP_DIR}" != "" ]; then
+        BOOTSTRAP_DIRS="${BOOTSTRAP_DIRS} ${CUSTOM_BOOTSTRAP_DIR}"
+    fi
 
-	BOOTSTRAP_DIRS=$(echo "${BOOTSTRAP_DIRS}" | sed 's/^ *//g;s/ *$//g;s/ {2}/ /g')
+    BOOTSTRAP_DIRS=$(echo "${BOOTSTRAP_DIRS}" | sed 's/^ *//g;s/ *$//g;s/ {2}/ /g')
 
-	for CHECK_DIR in ${BOOTSTRAP_DIRS}; do
-		test -d "${CHECK_DIR}" || (
-			echo "Missing bootstrap directory: [${CHECK_DIR}]"
-			exit 255
-		)
-	done
+    for CHECK_DIR in ${BOOTSTRAP_DIRS}; do
+        test -d "${CHECK_DIR}" || (
+            echo "Missing bootstrap directory: [${CHECK_DIR}]"
+            exit 255
+        )
+    done
 
-	find "${BOOTSTRAP_DIRS}" -type f | sort | while read -r BOOTSTRAP_SCRIPT_FILE; do
-		# shellcheck source=/dev/null
-		. "${BOOTSTRAP_SCRIPT_FILE}"
-	done
+    for BOOTSTRAP_SCRIPT_DIR in ${BOOTSTRAP_DIRS}; do
+        find "${BOOTSTRAP_SCRIPT_DIR}" -type f | sort | while read -r BOOTSTRAP_SCRIPT_FILE; do
+            # shellcheck source=/dev/null
+            . "${BOOTSTRAP_SCRIPT_FILE}"
+        done
+    done
 fi
 
 exec ${BOOTSTRAP_ENTRYPOINT:-${BOOTSTRAP_SHELL} -c} "$*"
