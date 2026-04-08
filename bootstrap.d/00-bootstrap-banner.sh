3ce45eb811562c37e73f553c04f1c22fd7794fa1
Ty <tycho.eggen@gmail.com>
2026-04-08 15:19:19 -0700

    chore(formatting): reformatted bootstrap.d/00-bootstrap-banner.sh

--- bootstrap.d/00-bootstrap-banner.sh
+++ bootstrap.d/00-bootstrap-banner.sh
@@ -5,12 +5,12 @@ BANNER="docker-bootstrap v${DOCKER_BOOSTRAP_VERSION:-0.0.0}"
 LEADER_LENGTH=$(((40 - ${#BANNER} - 2) / 2))
 
 repeat() {
-	local start=1
-	local end=${1:-80}
-	local str="${2:-=}"
-	local range=$(seq $start $end)
-	# shellcheck disable=SC2034
-	for i in $range; do echo -n "${str}"; done
+    local start=1
+    local end=${1:-80}
+    local str="${2:-=}"
+    local range=$(seq $start $end)
+    # shellcheck disable=SC2034
+    for i in $range; do echo -n "${str}"; done
 }
 
 echo "========================================"
